fad1109746a69989c1eca0400c2c36b32b759a05
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-10-31 23:15:58 +0530

    React-Router SetUp with createBrowserRouter

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,9 @@
+const Root = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
