8d0a17c81dc39a1bd3c54620fbc8cb93b25dd577
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-01 00:08:27 +0530

    DashBoard,Home,ErrorPage component and add to Root for router,add children for nested nevigation

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+const ErrorPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-5xl">Page not found</h1>
+      <p>
+        <small>Error:</small>404 not found
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Components/Root/Root";
+import ErrorPage from "./Components/Errorpage/Errorpage";
+import Home from "./Components/Home/Home";
+import DashBoard from "./Components/DashBoard/DashBoard";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "dashboard",
+        element: <DashBoard></DashBoard>,
+      },
+    ],
   },
 ]);
 
